3f100b418c1d5f20a7a0c2af869154e6a13aa678
ErickGomezUCA <00300723@uca.edu.sv>
2024-03-18 17:04:54 -0600

    Agregar menu de opciones en consola

--- Main.java
+++ Main.java
@@ -3,21 +3,50 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
 
-  }
+    int option = 0;
+    double result = 0;
+    Scanner sc = new Scanner(System.in);
 
-  private int add(int num1, int num2) {
-    return num1 + num2;
-  }
+    do {
+      System.out.print("[1] Add\n[2] Subtract\n[3] Multiply\n[4] Divide\n[0] Exit\n> ");
+      option = sc.nextInt();
 
-  private int subtract(int num1, int num2) {
-    return num1 - num2;
-  }
+      // Exit
+      if (option == 0) {
+        System.out.println("Exiting...");
 
-  private int multiply(int num1, int num2) {
-    return num1 * num2;
-  }
+        // Error handling
+      } else if (option < 1 && option > 4) {
+        System.out.println("Insert a valid option");
 
-  private int divide(int num1, int num2) {
-    return num1 / num2;
+      } else {
+        // Valid options
+        System.out.print("Insert first number:\n> ");
+        double firstNumber = sc.nextDouble();
+    
+        System.out.print("Insert second number:\n> ");
+        double secondNumber = sc.nextDouble();
+
+        switch(option) {
+          case 1:
+            result = Calculator.add(firstNumber, secondNumber);
+          break;
+    
+          case 2:
+            result = Calculator.subtract(firstNumber, secondNumber);
+            break;
+    
+          case 3:
+            result = Calculator.multiply(firstNumber, secondNumber);
+          break;
+    
+          case 4: 
+            result = Calculator.divide(firstNumber, secondNumber);
+          break;
+        }
+
+        System.out.printf("Result: %.2f\n", result);
+      }
+    } while (option != 0);
   }
 }
